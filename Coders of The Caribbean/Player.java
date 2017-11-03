0b0cd4e9bb95160b490943f192eea7fdb056f3af
BlyxxFR <blyxx70@gmail.com>
2017-11-03 20:17:40 +0100

    Update Player.java

--- Coders of The Caribbean/Player.java
+++ Coders of The Caribbean/Player.java
@@ -11,7 +11,7 @@ class Player {
     private static Scanner in;
     private static int myShipCount;
     private static int entityCount;
-    private static ArrayList<Ship> shipsList;
+    private static ArrayList<Ship> shipsList = new ArrayList<Ship>();
     private static ArrayList<Barrel> barrelsList;
 
     public static void main(String args[]) {
@@ -19,7 +19,6 @@ class Player {
 
         // game loop
         while (true) {
-            shipsList = new ArrayList<Ship>();
             barrelsList = new ArrayList<Barrel>();
             getData();
             
@@ -43,19 +42,32 @@ class Player {
         while(myActions.size() < myShipCount) {
             ActionType type = ActionType.WAIT;
             
-            // We search for the ships with <= 16 with ennemies
+            // We search for the ships with <= 3 with ennemies
             for (int i = 0; i < myShipCount; i++) {
                 Ship currentShip = shipsList.get(i);
                 if(!myActions.containsKey(currentShip)) {
                      for(int j = myShipCount; j < shipsList.size(); j++) {
                         Ship currentOtherShip = shipsList.get(j);
-                        double currentDist = getDistance2(currentShip.coordX, currentShip.coordY, currentOtherShip.coordX, currentOtherShip.coordY);
-                        
-                        System.err.println(currentDist);
-                        
-                        if(currentDist <= 16) {
-                            myActions.put(currentShip, new Action(ActionType.FIRE, currentOtherShip.coordX, currentOtherShip.coordY));
-                            break;
+                        if((barrelsList.size() > 0 && currentShip.stock > 20) || barrelsList.size() == 0) {
+                            double currentDist = getDistance2(currentShip.coordX, currentShip.coordY, currentOtherShip.coordX, currentOtherShip.coordY);
+                            
+                            System.err.println(currentDist);
+                            
+                            if(currentDist <= 2) {
+                                Barrel closestBarrel = findClosestBarrel(currentOtherShip, new ArrayList<Barrel>());
+                                
+                                // We try to predict the position of the ship
+                                System.err.println(currentShip.id + " " + currentOtherShip.id + " " + "Prev coord : " + currentOtherShip.prevCoordX + " " + currentOtherShip.prevCoordY + " --- " + currentOtherShip.coordX + " " + currentOtherShip.coordY);
+                                 
+                                int deltaX = currentOtherShip.coordX - currentOtherShip.prevCoordX;
+                                int deltaY = currentOtherShip.coordY - currentOtherShip.prevCoordY;
+                                
+                                int coordX = currentOtherShip.coordX + currentOtherShip.vitesse * deltaX;
+                                int coordY = currentOtherShip.coordY + currentOtherShip.vitesse * deltaY;
+                               
+                                myActions.put(currentShip, new Action(ActionType.FIRE, coordX, coordY));
+                                break;
+                            }
                         }
                     }
                 }
@@ -65,28 +77,31 @@ class Player {
             for (int i = 0; i < myShipCount; i++) {
                 Ship currentShip = shipsList.get(i);
                 if(!myActions.containsKey(currentShip)) {
-                    double dist = Integer.MAX_VALUE;
-                    Barrel closestBarrel = null;
-            
-                    for(int j = 0; j < barrelsList.size(); j++) {
-                        Barrel currentBarrel = barrelsList.get(j);
-                        if(!markedBarrel.contains(currentBarrel)) {
-                            double currentDist = getDistance2(currentShip.coordX, currentShip.coordY, currentBarrel.coordX, currentBarrel.coordY);
-                            if(dist > currentDist) {
-                                dist = currentDist;
-                                closestBarrel = currentBarrel;
-                            }
-                        }
-                    }
-                    
+                    Barrel closestBarrel = findClosestBarrel(currentShip, markedBarrel);
                     currentShip.closestBarrel = closestBarrel;
-                    if(closestBarrel == null) {
-                        myActions.put(currentShip, new Action(ActionType.WAIT, 0, 0));
-                    } else {
+                    if(closestBarrel != null) {
                         myActions.put(currentShip, new Action(ActionType.MOVE, closestBarrel.coordX, closestBarrel.coordY));
                         markedBarrel.add(closestBarrel);
                     }
                 }                    
+            }
+            
+            // If there is no barrel we move to closest ennemy
+            for (int i = 0; i < myShipCount; i++) {
+                Ship currentShip = shipsList.get(i);
+                if(!myActions.containsKey(currentShip)) {
+                    Ship closestShip = null;
+                    double dist = Integer.MAX_VALUE;
+                    for(int j = myShipCount; j < shipsList.size(); j++) {
+                        Ship currentOtherShip = shipsList.get(j);
+                        double currentDist = getDistance2(currentShip.coordX, currentShip.coordY, currentOtherShip.coordX, currentOtherShip.coordY);
+                        if(dist > currentDist) {
+                            dist = currentDist;
+                            closestShip = currentOtherShip;
+                        }
+                    }
+                    myActions.put(currentShip, new Action(ActionType.MOVE, closestShip.coordX, closestShip.coordY));
+                }
             }
              
         }
@@ -94,10 +109,27 @@ class Player {
         return myActions;
     }
     
+    private static Barrel findClosestBarrel(Ship currentShip, ArrayList<Barrel> markedBarrel) {
+        Barrel closestBarrel = null;
+        double dist = Integer.MAX_VALUE;
+        for(int j = 0; j < barrelsList.size(); j++) {
+            Barrel currentBarrel = barrelsList.get(j);
+            if(!markedBarrel.contains(currentBarrel)) {
+                double currentDist = getDistance2(currentShip.coordX, currentShip.coordY, currentBarrel.coordX, currentBarrel.coordY);
+                if(dist > currentDist) {
+                    dist = currentDist;
+                    closestBarrel = currentBarrel;
+                }
+            } 
+        }
+        return closestBarrel;
+    }
+    
+    
     private static enum ActionType { MOVE, SLOWER, WAIT, FIRE, MINE }
     
     private static double getDistance2(int coordX1, int coordY1, int coordX2, int coordY2) {
-        return Math.pow(coordX1 - coordX2, 2) + Math.pow(coordY1 - coordY2, 2);
+        return (Math.abs(coordX1 - coordX2) + Math.abs(coordY1 - coordY2)) / 2;
     }
     
     static class Action {
@@ -131,6 +163,7 @@ class Player {
     private static void getData() {
         myShipCount = in.nextInt(); // the number of remaining ships
         entityCount = in.nextInt(); // the number of entities (e.g. ships, mines or cannonballs)
+        ArrayList<Ship> tempShipsList = new ArrayList<Ship>();
         for (int i = 0; i < entityCount; i++) {
             int entityId = in.nextInt();
             String entityType = in.next();
@@ -144,7 +177,7 @@ class Player {
             switch(entityType) {
                 case "SHIP":
                     Ship ship = new Ship(entityId, x, y, arg1, arg2, arg3, arg4);
-                    shipsList.add(ship);
+                    tempShipsList.add(ship);
                     break;
                 case "BARREL":
                     Barrel barrel = new Barrel(entityId, x, y, arg1);
@@ -156,7 +189,19 @@ class Player {
                     break;
             }
             
-        }   
+        }
+        
+        // We get the previous xoords
+        for(int i = 0; i < tempShipsList.size(); i++) {
+            for(int j = i; j < shipsList.size(); j++) {
+                if(tempShipsList.get(i).id == shipsList.get(j).id) {
+                    tempShipsList.get(i).prevCoordX = shipsList.get(j).coordX;
+                    tempShipsList.get(i).prevCoordY = shipsList.get(j).coordY;
+                }
+            }
+        }
+        
+        shipsList = tempShipsList;
     }
     
     static class Ship {
@@ -166,6 +211,8 @@ class Player {
         public int vitesse;
         public int stock;
         public int player;
+        public int prevCoordX;
+        public int prevCoordY;
         public int coordX;
         public int coordY;
         public Barrel closestBarrel = null;
@@ -174,6 +221,8 @@ class Player {
             this.id = id;
             this.coordX = coordX;
             this.coordY = coordY;
+            this.prevCoordX = coordX;
+            this.prevCoordY = coordY;
             this.orientation = orientation;
             this.vitesse = vitesse;
             this.stock = stock;
